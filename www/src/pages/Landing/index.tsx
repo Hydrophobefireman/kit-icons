5712918be02f76e059a5e96ac3be63242143af31
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-10-09 16:53:58 -0400

    inline css

--- www/src/pages/Landing/index.tsx
+++ www/src/pages/Landing/index.tsx
@@ -3,52 +3,12 @@ import { css } from "catom";
 import { buttonStyle, innerButtonStyle } from "@/style";
 import { contains } from "@/util";
 import { useEffect, useState } from "@hydrophobefireman/ui-lib";
-import { AutoComplete } from "@kit/autocomplete";
 import { Button } from "@kit/button";
 import { Container } from "@kit/container";
 import * as $iconModule from "@kit/icons";
 import { Input } from "@kit/input";
 import { Text } from "@kit/text";
 
-const colorShades = `--kit-background
---kit-shade-1
---kit-shade-2
---kit-shade-3
---kit-shade-4
---kit-shade-5
---kit-shade-6
---kit-shade-7
---kit-shade-8
---kit-foreground
---kit-error-lightest
-  --kit-error-light
-  --kit-error
-  --kit-error-dark
-  --kit-success-lightest
-    --kit-success-light
-    --kit-success
-    --kit-success-dark
-    --kit-warning-lightest
-    --kit-warning-light
-    --kit-warning
-    --kit-warning-dark
-    --kit-violet-lightest
-    --kit-violet-light
-    --kit-violet
-    --kit-violet-dark
-     --kit-cyan-lightest
-    --kit-cyan-light
-    --kit-cyan
-    --kit-cyan-dark
-    --kit-highlight-purple
-    --kit-highlight-magenta
-    --kit-highlight-pink
-    --kit-highlight-yellow`
-  .split("\n")
-  .map((x) => ({
-    value: x.trim(),
-    render: (x) => <span>{x.replace(/-/g, " ")}</span>,
-  }));
 const icons = { ...$iconModule };
 const ic = Object.entries(icons);
 export default function Landing() {
